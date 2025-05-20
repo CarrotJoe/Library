05cf2806d65b2ab59378e9005fbe9bda18a98598
Joe <joe@joecomputers.co.uk>
2025-05-20 12:31:18 +0100

    set up database connection

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const connectDB = require('./config/db');
+const { addBook } = require('./controllers/addBook');
+var app = express();
+connectDB();
+
+
+
+
+app.post('/books', addBook);
+app.use(express.json());
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
